1459a3893741b95a11ad9df98506fd59718a76b8
Loser Teach <jetaimefrc@gmail.com>
2017-08-30 22:30:33 +0700

    Nhật quá đẹp trai <3

---
+++ Sources/shipperhola/src/main/java/app/ApplicationConstants.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright © 2017 XVideos Team
+ */
+package app;
+
+/**
+ *
+ * @author nhattq
+ */
+public class ApplicationConstants {
+    public static final String INDEX_VIEW_NAME = "index";
+}
