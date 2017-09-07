40712e98eae76e8f19c6a443199d921044c21681
vuphan365 <vuphan365@gmail.com>
2017-09-08 01:43:33 +0700

    Add demo code using view manager.

--- Sources/shipperhola/src/main/java/controller/IndexController.java
+++ Sources/shipperhola/src/main/java/controller/IndexController.java
@@ -1,16 +1,13 @@
 package controller;
 
-import java.util.HashMap;
 import spark.Route;
 import static app.Application.*;
-import java.util.Map;
 import static spark.Spark.*;
 import static app.ApplicationConstants.*;
 
 public class IndexController {
     public static final Route VIEW_INDEX_PAGE = (request, response) -> {
-        Map<String, Object> model = new HashMap<>();
-        return getTemplateEngine().render(modelAndView(model, "index"));
+        return getViewManager().renderForRequest(request, INDEX_VIEW_NAME);
     };
     
     public static void setupRoutes() {
